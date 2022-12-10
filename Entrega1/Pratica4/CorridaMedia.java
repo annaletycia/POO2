315271496a26494e98a1cf1d0e2aed58c7a07c33
Anna Letycia <anna.fernandes@ufu.br>
2022-12-10 15:16:20 -0300

    Add files via upload

---
+++ Entrega1/Pratica4/CorridaMedia.java
@@ -0,0 +1,7 @@
+//ConcreteStrategy
+public class CorridaMedia implements Corrida
+{
+    public void correr(){
+        System.out.println("Implementa corrida média.");
+    }
+}
---
+++ Entrega1/Pratica4/PuloMedio.java
@@ -0,0 +1,7 @@
+//ConcreteStrategy
+public class PuloMedio implements Pulo
+{
+    public void pular(){
+        System.out.println("Implementa pulo médio.");
+    }
+}
